0facfaead13a104ba9ce312c1db91ba11c204bfd
nrodofile <nrodofile@Nicholass-MacBook-Air.local>
2013-11-23 00:48:49 +1000

    addition of to logos and home view

--- ASIC_Small_Busnesss/gen/com/example/asic_small_busnesss/R.java
+++ ASIC_Small_Busnesss/gen/com/example/asic_small_busnesss/R.java
@@ -22,9 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int home_list_view=0x7f080001;
+        public static final int img_logo=0x7f080000;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int dsc_logo=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int home_txt_closing=0x7f050006;
         public static final int home_txt_complience=0x7f050005;
@@ -43,6 +48,7 @@ public final class R {
         public static final int home_txt_overview=0x7f050003;
         public static final int home_txt_resources=0x7f050007;
         public static final int home_txt_starting=0x7f050004;
+        public static final int home_txt_title=0x7f05000b;
         public static final int home_txt_topics=0x7f050008;
     }
     public static final class style {
